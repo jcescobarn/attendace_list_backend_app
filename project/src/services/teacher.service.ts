0dcb97889fcb69a00c649940887dd4b8b23e1659
jcescobarn <jcescobarn@unal.edu.co>
2023-02-02 19:41:24 -0500

    advance

--- project/src/services/teacher.service.ts
+++ project/src/services/teacher.service.ts
@@ -19,9 +19,27 @@ export default class TeacherService {
         return await this.teacherRepository.findOne({
             attributes: ["document_type","document_number","name","bachelor"],
             where:{
-                document_type: document_type,
-                document_number: document_number
+                document_type,
+                document_number
             }
+        });
+    }
+
+    public async delete(document_type:string, document_number:string):Promise<any>{
+        return await this.teacherRepository.destroy({
+            where:{
+                document_type,
+                document_number               
+            }
+        });
+    }
+
+    public async create(document_type:string,document_number:string,name:string,bachelor:string){
+        return await this.teacherRepository.create({
+            document_type,
+            document_number,
+            name,
+            bachelor
         });
     }
 
